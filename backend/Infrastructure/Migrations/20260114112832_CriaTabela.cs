32bea022921a9023ba15c01d7025dc539b5b73e3
joaobrambati <joaobrambati@gmail.com>
2026-01-14 08:58:58 -0300

    feat: cria DbSet para entidades e migration

---
+++ backend/Infrastructure/Migrations/20260114112832_CriaTabela.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriaTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cartoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NumeroCartaoHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UltimosQuatroDigitos = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: true),
+                    IdentificadorLinha = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: true),
+                    SequenciaLote = table.Column<int>(type: "int", nullable: true),
+                    CodigoLote = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true),
+                    DataLote = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OrigemCadastro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cartoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cartoes");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
