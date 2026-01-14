16f9f1f0a4cfdba3d514cad54ec2a025d908dc49
joaobrambati <joaobrambati@gmail.com>
2026-01-14 08:03:10 -0300

    feat: cria entidades

---
+++ backend/Domain/Entities/Cartao.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Cartao
+{
+    [Key]
+    public Guid Id { get; set; }
+    public required string NumeroCartaoHash { get; set; }
+
+    [MaxLength(4)]
+    public string? UltimosQuatroDigitos { get; set; }
+
+    [MaxLength(1)]
+    public string? IdentificadorLinha { get; set; }
+
+    public int? SequenciaLote { get; set; }
+
+    [MaxLength(8)]
+    public string? CodigoLote { get; set; }
+
+    public DateTime? DataLote { get; set; }
+    public required string OrigemCadastro { get; set; }
+    public DateTime CriadoEm { get; set; } = DateTime.Now;
+}
