fa1c791c3972b8a75183d90b587ee6706e3cbbbb
joaobrambati <joaobrambati@gmail.com>
2026-01-15 11:05:37 -0300

    feat: remove data annotations nao sendo utilizados

--- backend/Domain/Entities/Cartao.cs
+++ backend/Domain/Entities/Cartao.cs
@@ -7,18 +7,10 @@ public class Cartao
     [Key]
     public Guid Id { get; set; }
     public required string NumeroCartaoHash { get; set; }
-
-    [MaxLength(4)]
     public string? UltimosQuatroDigitos { get; set; }
-
-    [MaxLength(1)]
     public string? IdentificadorLinha { get; set; }
-
     public string? SequenciaLote { get; set; }
-
-    [MaxLength(8)]
     public string? CodigoLote { get; set; }
-
     public DateTime? DataLote { get; set; }
     public required string OrigemCadastro { get; set; }
     public DateTime CriadoEm { get; set; } = DateTime.Now;
---
+++ backend/Infrastructure/Migrations/20260115134357_RemocaoDataAnnotationsCartao.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemocaoDataAnnotationsCartao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UltimosQuatroDigitos",
+                table: "Cartoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentificadorLinha",
+                table: "Cartoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldMaxLength: 1,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoLote",
+                table: "Cartoes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UltimosQuatroDigitos",
+                table: "Cartoes",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentificadorLinha",
+                table: "Cartoes",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoLote",
+                table: "Cartoes",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
