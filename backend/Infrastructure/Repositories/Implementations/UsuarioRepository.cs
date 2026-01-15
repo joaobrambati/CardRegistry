2fab8bb5a53daf5f4cfdf5f0e6b9c7a99e3c015e
joaobrambati <joaobrambati@gmail.com>
2026-01-15 14:39:51 -0300

    feat: repositório de Usuario

---
+++ backend/Infrastructure/Repositories/Implementations/UsuarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UsuarioAuth?> GetByEmail(string email)
+        => await _context.UsuariosAuth.FirstOrDefaultAsync(u => u.Email == email);
+
+    public async Task Create(UsuarioAuth usuario)
+        => await _context.UsuariosAuth.AddAsync(usuario);
+
+    public async Task SaveChangesAsync()
+        => await _context.SaveChangesAsync();
+
+}
---
+++ backend/Infrastructure/Repositories/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface IUsuarioRepository
+{
+    Task<UsuarioAuth?> GetByEmail(string email);
+    Task Create(UsuarioAuth usuario);
+    Task SaveChangesAsync();
+}
